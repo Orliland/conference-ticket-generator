65a0b4f7a4c7d94e8764667c505b3ca5b830bbf6
Orlando Verdin <git@orli.land>
2024-12-19 21:10:24 -0700

    create button component and add styles

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+export const Button = () => {
+  return (
+    <button className="text-preset-5 extrabold hover:shadow-border-bottom focus:shadow-border w-full rounded-12 bg-orange-500 px-300 py-200 text-neutral-900 hover:bg-orange-700 focus:outline-none">
+      Generate My Ticket
+    </button>
+  );
+};
